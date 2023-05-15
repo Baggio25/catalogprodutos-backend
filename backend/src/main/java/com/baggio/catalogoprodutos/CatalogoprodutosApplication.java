3e2d75c6a6dd2959bddfe9cdba50ac2d922982c9
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-15 11:17:04 -0300

    Backend project create

---
+++ backend/src/main/java/com/baggio/catalogoprodutos/CatalogoprodutosApplication.java
@@ -0,0 +1,13 @@
+package com.baggio.catalogoprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoprodutosApplication.class, args);
+	}
+
+}
