2792e5319ee09bdc3d7f2cf7ef08dba03e94c5c8
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-15 15:58:58 -0300

    Find category by id

--- backend/src/main/java/com/baggio/catalogoprodutos/resource/CategoryResources.java
+++ backend/src/main/java/com/baggio/catalogoprodutos/resource/CategoryResources.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class CategoryResources {
 	public ResponseEntity<Page<CategoryDTO>> findAllCategories(Pageable pageable) {
 		Page<CategoryDTO> pageDTO = categoryService.findAll(pageable);		
 		return ResponseEntity.ok(pageDTO);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+		CategoryDTO categoryDTO = categoryService.findById(id);		
+		return ResponseEntity.ok(categoryDTO);
 	}
 	
 }
--- backend/src/main/java/com/baggio/catalogoprodutos/service/CategoryService.java
+++ backend/src/main/java/com/baggio/catalogoprodutos/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.baggio.catalogoprodutos.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.baggio.catalogoprodutos.dto.CategoryDTO;
 import com.baggio.catalogoprodutos.entity.Category;
@@ -15,9 +18,17 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public Page<CategoryDTO> findAll(Pageable pageable) {
 		Page<Category> pageDTO = categoryRepository.findAll(pageable); 
 		return pageDTO.map(category -> new CategoryDTO(category));		
 	}
 	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> categoryOptional = categoryRepository.findById(id);
+		Category category = categoryOptional.get();
+
+		return new CategoryDTO(category);
+	}
 }
