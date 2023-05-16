4a9423548268e57938d831990e6f1673287c849d
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-16 15:12:02 -0300

    Product insert, update, delete

--- backend/src/main/java/com/baggio/catalogoprodutos/service/ProductService.java
+++ backend/src/main/java/com/baggio/catalogoprodutos/service/ProductService.java
@@ -12,9 +12,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.baggio.catalogoprodutos.dto.CategoryDTO;
 import com.baggio.catalogoprodutos.dto.ProductDTO;
 import com.baggio.catalogoprodutos.dto.ProductListDTO;
+import com.baggio.catalogoprodutos.entity.Category;
 import com.baggio.catalogoprodutos.entity.Product;
+import com.baggio.catalogoprodutos.repository.CategoryRepository;
 import com.baggio.catalogoprodutos.repository.ProductRepository;
 import com.baggio.catalogoprodutos.service.exceptions.DatabaseException;
 import com.baggio.catalogoprodutos.service.exceptions.ResourceNotFoundException;
@@ -25,6 +28,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
 	public Page<ProductListDTO> findAll(Pageable pageable) {
@@ -44,19 +50,20 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO productDTO) {
 		Product product = new Product();
+		copyDtoToEntity(productDTO, product);
 		product = productRepository.save(product);
 		
-		return new ProductDTO(product);
+		return new ProductDTO(product, product.getCategories());
 	}
 
 	@Transactional
 	public ProductDTO update(ProductDTO productDTO, Long id) {
 		try {
 			Product product = productRepository.getReferenceById(id);
-						
+			copyDtoToEntity(productDTO, product);	
 			product = productRepository.save(product);
 			
-			return new ProductDTO(product);
+			return new ProductDTO(product, product.getCategories());
 		}catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Entidade não encontrada com o id: "+ id);
 		}
@@ -73,6 +80,19 @@ public class ProductService {
 		}
 		
 	}
-	
+
+	private void copyDtoToEntity(ProductDTO productDTO, Product product) {
+		product.setName(productDTO.getName());
+		product.setDescription(productDTO.getDescription());
+		product.setPrice(productDTO.getPrice());
+		product.setDate(productDTO.getDate());
+		product.setImgUrl(productDTO.getImgUrl());
+		
+		product.getCategories().clear();
+		for(CategoryDTO categoryDTO : productDTO.getCategories()) {
+			Category category = categoryRepository.getReferenceById(categoryDTO.getId());
+			product.getCategories().add(category);
+		}
+	}
 	
 }
