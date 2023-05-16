9d3482763d92cf821013e5199c20c037ef483312
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-16 09:17:55 -0300

    DELETE Category

--- backend/src/main/java/com/baggio/catalogoprodutos/service/CategoryService.java
+++ backend/src/main/java/com/baggio/catalogoprodutos/service/CategoryService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.baggio.catalogoprodutos.dto.CategoryDTO;
 import com.baggio.catalogoprodutos.entity.Category;
 import com.baggio.catalogoprodutos.repository.CategoryRepository;
+import com.baggio.catalogoprodutos.service.exceptions.DatabaseException;
 import com.baggio.catalogoprodutos.service.exceptions.ResourceNotFoundException;
 
 
@@ -57,6 +60,18 @@ public class CategoryService {
 		}catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Entidade não encontrada com o id: "+ id);
 		}
+	}
+
+	public void update(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Entidade não encontrada com o id: "+ id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade, já existe uma entidade "
+					+ "utilizando esse registro");
+		}
+		
 	}
 	
 	
