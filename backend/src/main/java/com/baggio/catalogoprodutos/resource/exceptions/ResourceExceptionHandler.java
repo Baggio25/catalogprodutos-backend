71a8a406231be69eaab23bbd20fc5a595e756c0b
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-17 09:18:35 -0300

    Exception MethodArgumentNoValidException

--- backend/src/main/java/com/baggio/catalogoprodutos/resource/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/baggio/catalogoprodutos/resource/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -44,5 +45,21 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(status).body(error);
 	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Validation Exception");
+		error.setMessage(ex.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+
 	
 }
