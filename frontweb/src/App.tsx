a140d798da16be121fe135cbff90426099f56d30
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-18 14:31:43 -0300

    Project Clean

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,8 @@
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Teste
-      </header>
-    </div>
+    <span>DSCatalog</span>
   );
 }
 
