e1eae70f39851e4cb0518d3d05366381a80e7e21
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-19 11:33:29 -0300

    Home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,20 +1,24 @@
+import ButtonIcon from '../../components/ButtonIcon';
+import { ReactComponent as MainImage } from '../../assets/images/main-image.svg';
 
-import ButtonIcon from "../../components/ButtonIcon";
-import { ReactComponent as MainImage } from "../../assets/images/main-image.svg";
-
-import "./styles.css";
+import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-card">
         <div className="home-content-container">
+          <div>
             <h1>Conheça o melhor catálogo de produtos</h1>
-            <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
-            <ButtonIcon />
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado
+            </p>
+          </div>
+          <ButtonIcon />
         </div>
         <div className="home-image-container">
-            <MainImage />
+          <MainImage />
         </div>
       </div>
     </div>
