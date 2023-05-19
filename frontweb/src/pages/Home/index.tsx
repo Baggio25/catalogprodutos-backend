9c74d7a6d778cd3d76b01f12dfc72e9e61a14395
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-19 09:23:44 -0300

    Layou x-small finished

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,12 +1,17 @@
 
+import ButtonIcon from "../../components/ButtonIcon";
 import { ReactComponent as MainImage } from "../../assets/images/main-image.svg";
+
+import "./styles.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-card">
         <div className="home-content-container">
-            <h2>Conhceça o melhor catálogo de produtos</h2>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
+            <ButtonIcon />
         </div>
         <div className="home-image-container">
             <MainImage />
