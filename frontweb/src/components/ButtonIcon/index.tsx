6c9d45150cfffc292262b7a2fec6ac97f2e39375
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-22 11:42:22 -0300

    Cutom ButtonIcon

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,14 +1,18 @@
+import { ReactComponent as Arrow } from '../../assets/images/arrow.svg';
 
-import "./styles.css";
+import './styles.css';
 
 const ButtonIcon = () => {
-    return(
-        <>
-            <button className="btn btn-primary btn-icon">
-                <h6>INICIE AGORA A SUA BUSCA</h6>
-            </button>
-        </>
-    )
-}
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
+      <div className="btn-icon-container">
+        <Arrow />
+      </div>
+    </div>
+  );
+};
 
 export default ButtonIcon;
