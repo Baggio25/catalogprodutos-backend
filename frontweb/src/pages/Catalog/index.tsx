bf377d2cabc9ddde720b9cf672ebf001acc2e65e
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-22 15:29:00 -0300

    Format Price

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -3,22 +3,24 @@ import ProductCard from '../../components/ProductCard';
 
 const Catalog = () => {
   const product: Product = {
-    id: 27,
-    name: 'Xbox series s',
-    description: 'The new generation Xbox video game',
-    price: 2360.0,
-    imgUrl: 'The new generation Xbox video game',
-    date: '2023-05-16T14:56:00Z',
+    id: 2,
+    name: 'Smart TV',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 2190.0,
+    imgUrl:
+      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
+    date: '2022-07-14T10:00:00Z',
     categories: [
       {
-        id: 5,
-        name: 'Game - Console',
+        id: 1,
+        name: 'Livros',
       },
       {
-        id: 2,
-        name: 'Eletrônicos',
+        id: 3,
+        name: 'Computadores',
       },
-    ],  
+    ],
   };
 
   return (
