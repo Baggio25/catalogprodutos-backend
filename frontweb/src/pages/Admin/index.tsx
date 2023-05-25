c6344414f011eec7e28a6c5023297eb7e78c41fa
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-25 10:42:09 -0300

    Private Route

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,32 +1,33 @@
-import { Route, Switch } from "react-router-dom";
+import { Switch } from 'react-router-dom';
 
-import NavbarAdmin from "./NavbarAdmin";
-import Users from "./User";
+import PrivateRoute from 'components/PrivateRoute';
+import NavbarAdmin from './NavbarAdmin';
+import Users from './User';
 
-import "./styles.css";
+import './styles.css';
 
 const Admin = () => {
-    return(
-        <div className="admin-container">
-            <NavbarAdmin />
-            <div className="admin-content">
-                <Switch>
-                    <Route path="/admin/dashboard">
-                        <h1>Dashboard</h1>
-                    </Route>
-                    <Route path="/admin/products">
-                        <h1>Product CRUD</h1>
-                    </Route>
-                    <Route path="/admin/categories">
-                        <h1>Category CRUD</h1>
-                    </Route>
-                    <Route path="/admin/users">
-                        <Users />
-                    </Route>
-                </Switch>
-            </div>  
-        </div>
-    )
-}
+  return (
+    <div className="admin-container">
+      <NavbarAdmin />
+      <div className="admin-content">
+        <Switch>
+          <PrivateRoute path="/admin/dashboard">
+            <h1>Dashboard</h1>
+          </PrivateRoute>
+          <PrivateRoute path="/admin/products">
+            <h1>Product CRUD</h1>
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
+            <h1>Category CRUD</h1>
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users">
+            <Users />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
