5a10567f4a842bc517a10f463f9504c9c74fb4db
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-26 16:27:28 -0300

    hasAnyRoles

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -87,6 +87,19 @@ export const isAuthenticated = () : boolean => {
   return (tokenData && ((tokenData.exp * 1000) > Date.now())) ? true : false;
 }
 
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+  if(roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+  if(tokenData !== undefined) {
+    return roles.some(role => tokenData.authorities.includes(role));
+  }
+
+  return false;
+}
+
 axios.interceptors.request.use(function (config) {
   return config;
 }, function (error) {
