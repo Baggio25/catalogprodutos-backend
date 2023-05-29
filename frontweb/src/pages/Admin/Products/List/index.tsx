bdeb4a20a10b1a11ebeb3efab7ff7fe8441f6619
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-29 16:01:19 -0300

    Product crud form html

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -23,7 +23,7 @@ const List = () => {
   };
 
   return (
-    <>
+    <div className="product-crud-container">
       <div className="product-crud-bar-container">
         <Link to="/admin/products/create">
           <button className="btn btn-primary text-with btn-crud-add">
@@ -44,7 +44,7 @@ const List = () => {
         </div>
        
       </div>
-    </>
+    </div>
   );
 };
 
